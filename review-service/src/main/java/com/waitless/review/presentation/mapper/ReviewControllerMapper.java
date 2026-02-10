59d739906e20591b47e5bc5917c1ae969a2d6977
2sminn <sungmingod13@gmail.com>
2026-02-10 21:43:46 +0900

    feat(review): 리뷰수정API를 위한 mapper추가, result추가

--- review-service/src/main/java/com/waitless/review/presentation/mapper/ReviewControllerMapper.java
+++ review-service/src/main/java/com/waitless/review/presentation/mapper/ReviewControllerMapper.java
@@ -4,6 +4,7 @@ import com.waitless.common.domain.UserInfoDto;
 import com.waitless.review.application.dto.command.DeleteReviewCommand;
 import com.waitless.review.application.dto.command.PostReviewCommand;
 import com.waitless.review.application.dto.command.ReviewStatisticsCommand;
+import com.waitless.review.application.dto.command.UpdateReviewCommand;
 import com.waitless.review.domain.vo.ReviewSearchCondition;
 import com.waitless.review.presentation.dto.request.*;
 import org.mapstruct.Mapper;
@@ -24,14 +25,25 @@ public interface ReviewControllerMapper {
     @Mapping(source = "userInfoDto.userId", target = "userId")
     @Mapping(source = "userInfoDto", target = "userInfoDto")
     DeleteReviewCommand toCommand(DeleteReviewRequestDto requestDto, UserInfoDto userInfoDto);
+
+    @Mapping(source = "requestDto.reviewId", target = "reviewId")
+    @Mapping(source = "requestDto.content", target = "content")
+    @Mapping(source = "requestDto.rating", target = "rating")
+    @Mapping(source = "userInfoDto.userId", target = "userId")
+    @Mapping(source = "userInfoDto", target = "userInfoDto")
+    UpdateReviewCommand toCommand(UpdateReviewRequestDto requestDto, UserInfoDto userInfoDto);
+
     default ReviewSearchCondition toCondition(GetReviewRequestDto requestDto) {
         return new ReviewSearchCondition(requestDto.reviewId(), null, null, null);
     }
+
     default ReviewSearchCondition toCondition(GetReviewListRequestDto requestDto) {
         return new ReviewSearchCondition(null, requestDto.userId(), requestDto.restaurantId(), null);
     }
+
     default ReviewSearchCondition toCondition(SearchReviewsRequestDto requestDto) {
         return new ReviewSearchCondition(null, requestDto.userId(), requestDto.restaurantId(), requestDto.rating());
     }
+
     ReviewStatisticsCommand toCommand(ReviewStatisticsBatchRequestDto requestDto, UserInfoDto userInfoDto);
 }
